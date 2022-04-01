94367c5c262c2ce26cc1434f9de7dbea7c0f61ca
hayyoung <vanilahay@gmail.com>
2022-04-01 22:51:02 +0900

    home frame

---
+++ lib/Home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+  appBar: AppBar(
+    title: const Text('BottomNavigationBar Demo'),
+  ),
+  bottomNavigationBar: BottomNavigationBar(
+    items: const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.call),
+        label: 'Home',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.camera),
+        label: 'Chats',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.chat),
+        label: 'profile',
+      ),
+    ],
+  ),
+);
+  }
+ 
